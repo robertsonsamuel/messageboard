80f5f65528ae8254e300fcab6587e79112f13441
Cade <cade@Sankys-MacBook-Pro.local>
2015-11-09 21:34:37 -0800

    working

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
+var marked = require('marked');
+
 var app = express();
 
 app.set('view engine', 'jade');
@@ -17,6 +19,14 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.render('index');
+});
+
+app.get('/markdown', function(req, res) {
+  res.render('markdown');
+});
+
+app.post('/markdown', function(req, res) {
+  res.send(marked(req.body.md));
 });
 
 app.listen(PORT, function(){
--- public/js/main.js
+++ public/js/main.js
@@ -3,5 +3,17 @@
 $(document).ready(init);
 
 function init() {
-  console.log('jQuery working!');
+  $('#convert').click(convertMarkdown);
+}
+
+function convertMarkdown() {
+  var md = $('#input').val();
+  $.post('/markdown', {md: md})
+  .done(function(data) {
+    var $converted = $.parseHTML(data);
+    $('#output').empty().append($converted);
+  })
+  .fail(function(err) {
+    console.error(err);
+  });
 }
